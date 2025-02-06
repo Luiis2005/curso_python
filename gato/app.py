f6879003e85bcdfa9c6fa21bbc4958bede134ba2
Luiis2005 <orrantiajorge26@gmail.com>
2025-02-06 14:26:39 -0700

    Cambios

--- gato/app.py
+++ gato/app.py
@@ -1,16 +1,31 @@
 import tablero
 
 def main():
-    numeros = [str(i) for i in range(1,10)]	
-    dsimbolos = {x:x for x in numeros}
-    tablero.juego(dsimbolos)
-    g = tablero.juego(dsimbolos)
-    if g is not None:
-        print(f'Gana: {g}')
-    else:
-        print('Empate')
-
-
-print(__name__)
+    X = {"G":0, "P":0, "E":0}
+    O = {"G":0, "P":0, "E":0}
+    score = {"X":X, "O":O}
+    numeros = [str(i) for i in range(1,10)]
+    corriendo = True
+    while corriendo:
+        dsimbolos = {x:x for x in numeros}
+        g = tablero.juego(dsimbolos)
+        if g is not None:
+            if g == "X":
+                X["G"] += 1
+                O["P"] += 1
+            elif g == "O":
+                O["G"] += 1
+                X["P"] += 1
+            else:
+                X["E"] += 1
+                O["E"] += 1
+        else:
+            print("Empate")
+            X["E"] += 1
+            O["E"] += 1
+            print("Ganador: ", g)
+            print("Score: ", score)
+            corriendo = False	
+        print("Score: ", score)
 if __name__ == '__main__':
     main()
